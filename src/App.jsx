f186a68e8222a398c641d35d9656a325f9d65a04
BhuwanArya <aryabhuwan873@gmail.com>
2023-10-12 13:02:14 +0530

    Add:welcome to the geektrustAdmin-ui

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Table from "./components/content";
+import "./app.css";
+
+function App() {
+
+  return (
+   <div>
+    <h1>
+      UserDashboard
+    </h1>
+    <Table/>
+   </div>
+  )
+}
+
+export default App
